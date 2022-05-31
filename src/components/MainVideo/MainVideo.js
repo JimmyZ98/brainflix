cbe35e0d4a892cfd9414472e1a04831e224b8f1f
JimmyZ98 <sam.jimmy.zhen@gmail.com>
2022-05-31 02:49:39 -0400

    updated tablet view styling for video details and comments

--- src/components/MainVideo/MainVideo.js
+++ src/components/MainVideo/MainVideo.js
@@ -4,7 +4,11 @@ import React from "react";
 function MainVideo(props) {
   return (
     <section className="main">
-      <video controls poster={props.selectedVideo.image} className="main-video">
+      <video
+        controls
+        poster={props.selectedVideo.image}
+        className="main__video"
+      >
         <source src={props.selectedVideo.video + "?api_key=test"} />
       </video>
     </section>
