45f668dd76d90ebf5e1dc9bfb198e8e92bf8b408
JimmyZ98 <sam.jimmy.zhen@gmail.com>
2022-06-06 03:58:51 -0400

    implemented linking/alert for publish button, updated scss styling for tablet and desktop view

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import "./UploadPage.scss";
+import { Link } from "react-router-dom";
 
 function UploadPage() {
+  const uploadAlert = () => {
+    alert("Upload successful");
+  };
+
   return (
     <>
       <Header />
@@ -11,44 +16,57 @@ function UploadPage() {
           <h1 className="upload__title">Upload Video</h1>
 
           <form className="upload__form">
-            <div className="upload__form-group">
-              <label className="upload__form-label" htmlFor="videoThumbnail">
-                VIDEO THUMBNAIL
-              </label>
-              <figure
-                className="upload__form-thumbnail"
-                id="videoThumbnail"
-                name="videoThumbnail"
-              ></figure>
+            <div className="upload__inputs">
+              <div className="upload__form-group upload__form--left">
+                <label className="upload__form-label" htmlFor="videoThumbnail">
+                  VIDEO THUMBNAIL
+                </label>
+                <figure
+                  className="upload__form-thumbnail"
+                  id="videoThumbnail"
+                  name="videoThumbnail"
+                ></figure>
+              </div>
+              <div className="upload__form--right">
+                <div className="upload__form-group">
+                  <label className="upload__form-label" htmlFor="videoTitle">
+                    TITLE YOUR VIDEO
+                  </label>
+                  <input
+                    className="upload__form-input"
+                    id="videoTitle"
+                    name="videoTitle"
+                    type="text"
+                    placeholder="Add a title to your video"
+                  />
+                </div>
+                <div className="upload__form-group">
+                  <label
+                    className="upload__form-label"
+                    htmlFor="videoDescription"
+                  >
+                    ADD A VIDEO DESCRIPTION
+                  </label>
+                  <input
+                    className="upload__form-input--tall"
+                    id="videoDescription"
+                    name="videoDescription"
+                    type="text"
+                    placeholder="Add a description to your video"
+                  />
+                </div>
+              </div>
             </div>
-            <div className="upload__form-group">
-              <label className="upload__form-label" htmlFor="videoTitle">
-                TITLE YOUR VIDEO
-              </label>
-              <input
-                className="upload__form-input"
-                id="videoTitle"
-                name="videoTitle"
-                type="text"
-                placeholder="Add a title to your video"
-              />
-            </div>
-            <div className="upload__form-group">
-              <label className="upload__form-label" htmlFor="videoDescription">
-                ADD A VIDEO DESCRIPTION
-              </label>
-              <input
-                className="upload__form-input--tall"
-                id="videoDescription"
-                name="videoDescription"
-                type="text"
-                placeholder="Add a description to your video"
-              />
-            </div>
-            <div className="upload__form--submit-container">
-              <button type="submit" className="upload__form-submit">
+            <div className="upload__form-submit--container">
+              <Link
+                to="/"
+                type="submit"
+                className="upload__form-submit"
+                onClick={uploadAlert}
+              >
                 PUBLISH
-              </button>
+              </Link>
+              <p className="upload__form-cancel">CANCEL</p>
             </div>
           </form>
         </div>
