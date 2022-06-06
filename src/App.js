c1d2177772b13e79d911b137ff1fa21a2423cad3
JimmyZ98 <sam.jimmy.zhen@gmail.com>
2022-06-05 21:17:30 -0400

    created HomePage, reformatted javascript to pull data from API

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import "./App.scss";
-import { Component } from "react";
-import videos from "./data/videos.json";
-import videoDetails from "./data/video-details.json";
-import Header from "./components/Header/Header";
-import MainVideo from "./components/MainVideo/MainVideo";
-import PageBottom from "./components/PageBottom/PageBottom";
+import React from "react";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-class App extends Component {
-  state = {
-    videos,
-    videoDetails,
-    selectedVideo: videoDetails[0],
-  };
-
-  handleClick = (id) => {
-    let chosenVideo = this.state.videoDetails.find((video) => video.id === id);
-    this.setState({
-      selectedVideo: chosenVideo,
-    });
-  };
-
-  render() {
-    return (
-      <div className="wrapper">
-        <Header />
-        <MainVideo selectedVideo={this.state.selectedVideo} />
-        <PageBottom
-          videos={this.state.videos}
-          selectedVideo={this.state.selectedVideo}
-          handleClick={this.handleClick}
-        />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/video/:videoId" component={HomePage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
-
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,76 @@
+import { Component } from "react";
+import videos from "../data/videos.json";
+import videoDetails from "../data/video-details.json";
+import Header from "../components/Header/Header";
+import MainVideo from "../components/MainVideo/MainVideo";
+import PageBottom from "../components/PageBottom/PageBottom";
+import axios from "axios";
+
+const API_URL = "https://project-2-api.herokuapp.com";
+const API_KEY_STRING = "?api_key=e82413f2-dc28-4f4e-8bb9-704af807b4f4";
+
+class HomePage extends Component {
+  constvi;
+  state = {
+    videos: [],
+    selectedVideo: videoDetails[0],
+    invalidVideo: false,
+  };
+
+  componentDidMount() {
+    console.log("component did mount");
+
+    axios.get(`${API_URL}/videos${API_KEY_STRING}`).then((response) => {
+      this.setState({
+        videos: response.data,
+      });
+
+      const videoId = this.props.match.params.videoId || response.data[0].id;
+      console.log(videoId);
+
+      this.getSelectedVideo(videoId);
+    });
+    console.log(videos);
+  }
+
+  componentDidUpdate(previousProps) {
+    const previousVideoId = previousProps.match.params.videoId;
+    const currentVideoId = this.props.match.params.videoId;
+
+    if (previousVideoId !== currentVideoId) {
+      this.getSelectedVideo(currentVideoId);
+    }
+  }
+
+  getSelectedVideo = (videoId) => {
+    axios
+      .get(`${API_URL}/videos/${videoId}${API_KEY_STRING}`)
+      .then((response) => {
+        this.setState({
+          selectedVideo: response.data,
+          invalidVideo: false,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          invalidVideo: true,
+        });
+      });
+  };
+
+  render() {
+    console.log("render");
+    return (
+      <div className="wrapper">
+        <Header />
+        <MainVideo selectedVideo={this.state.selectedVideo} />
+        <PageBottom
+          videos={this.state.videos}
+          selectedVideo={this.state.selectedVideo}
+        />
+      </div>
+    );
+  }
+}
+
+export default HomePage;
